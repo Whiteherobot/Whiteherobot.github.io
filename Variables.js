c18ab233c481c245c1253ece8a57eb777790378f
SirYorch <jcuevam3@est.ups.edu.ec>
2025-02-10 13:21:10 -0500

    Organizacion de archivos .js

---
+++ Variables.js
@@ -0,0 +1,5 @@
+let formularioPersona = document.getElementById('person-form');
+
+let formularioRelacion = document.getElementById('relationship-form');
+
+let botonRelaciones = document.querySelectorAll('.accordion')[3];
---
+++ onInit.js
@@ -0,0 +1,6 @@
+
+
+// Llamar a esta función al cargar la página para asegurar que los selectores estén actualizados
+document.addEventListener('DOMContentLoaded', function() {
+    loadPersonOptions();
+});
