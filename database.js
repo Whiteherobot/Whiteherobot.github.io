8d0fa66aef7ff6246f2c4d3ec7440689c1854fd4
Michi <mlata6235@gmail.com>
2025-02-10 12:52:59 -0500

    no
    zzz

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const neo4j = window.neo4j;
 
-const driver = neo4j.driver('bolt://localhost:7687', neo4j.auth.basic('neo4j', 'Vp60-h15jP'));
+const driver = neo4j.driver('bolt://localhost:7687', neo4j.auth.basic('neo4j', '12345678'));
 const session = driver.session();
 
 function close() {
